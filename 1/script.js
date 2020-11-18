6916df6c966a3962aa6003aad9e7180f6d9cfccf
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-11-18 13:46:23 +0100

    uppgift 1

--- 1/script.js
+++ 1/script.js
@@ -1,25 +1,28 @@
 
 // 1. Deklarera en variabel a och tilldela den värdet 5. Byt sedan värdet på a till 8
+let a = 5
+a = 8
 
 // 2. Deklarera en konstant variabel tilldela den värdet av ditt förnamn
+const name = Emma
 
 // 3. 
-// a) Hur många argument tar den här funktionen? SVAR: ____
-// b) Vilken datatyp returnerar den? SVAR: _____
+// a) Hur många argument tar den här funktionen? SVAR: 0
+// b) Vilken datatyp returnerar den? SVAR: sträng
 function foo() {
     return "foo"
 }
 
 // 4. 
-// a) Hur många argument tar den här funktionen? SVAR: ____
-// b) Vilken datatyp returnerar den? SVAR: _____
+// a) Hur många argument tar den här funktionen? SVAR: 1
+// b) Vilken datatyp returnerar den? SVAR: array (eller beror på vad man stoppar in som argument)
 function bar(arr) {
     return arr
 }
 
 // 5.
-// a) Hur många argument tar den här funktionen? SVAR: ____
-// b) Vilken datatyp returnerar den? SVAR: _____
+// a) Hur många argument tar den här funktionen? SVAR: 3
+// b) Vilken datatyp returnerar den? SVAR: den returnerar inget
 function bar(a, b, c) {
     console.log(a)
 }
