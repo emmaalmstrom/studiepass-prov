9f5d28ef6202d8f700775a41c99970fe8aa1d4ee
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-11-18 13:49:45 +0100

    uppgift 2

--- 2/script.js
+++ 2/script.js
@@ -1,12 +1,13 @@
 
 // 1. Varför loggas ingenting ut när man kör det här skriptet? 
-// Det finns två fel. SVAR: ________
+// Det finns två fel. SVAR: för att vi aldring kör funtionen. Och för att det är fel datatyp på rad 8
 
 // 2. Ändra scriptet så att "Hello, how are you today?" verkligen loggas ut.
 
-function hello() {
-    console.log(Hello, how are you today?)
+function hello(name) {
+    console.log(`Hello ${name}, how are you today?`)
 }
 
+hello('Einar')
 // 3. Uppdatera funktionen hello så att den tar ett namn som argument
 // Gör så att den hälsar med namn, t.ex. "Hello Fredrik, how are you today?"
