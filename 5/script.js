0efdb6f74f8e75dab2621c8728a5f4b99bac552d
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-11-20 08:51:15 +0100

    uppgift 4 och 5

---
+++ 5/script.js
@@ -0,0 +1,8 @@
+function returnLastElement(arr) {
+    const lastElement = arr.length - 1
+    return arr[lastElement]
+}
+
+const bilar = ['volksvagen', 'BMW', 'Volvo', 'Skoda', 'Tesla']
+
+console.log(returnLastElement(bilar))
